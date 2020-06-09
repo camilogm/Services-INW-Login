b031d1561fe587f99f1f6ba497e6deb76ca02a3a
Camilo Gonz <camilogit123@gmail.com>
2020-06-09 17:21:18 -0600

    Adding failed resposne

--- src/main/java/com/inw/filters/JwtRequestFilter.java
+++ src/main/java/com/inw/filters/JwtRequestFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -40,7 +41,13 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 	    
 	        	jwt = authorizationHeader.substring(7);
 	            userName = jwtUtil.extractUsername(jwt);
-	        } 
+	        } else {
+	        	response.reset();
+	        	response.setStatus(HttpStatus.BAD_REQUEST.value());
+	        	response.getWriter().flush();
+	        	return;
+	        	
+	        }
 
 	        if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 
