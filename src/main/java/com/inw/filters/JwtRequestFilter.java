f2c3ee2ed1f5e9027ec37289279f3c16db254cb6
Camilo Gonz <camilogit123@gmail.com>
2020-06-13 20:35:44 -0600

    adding cors and order filters

--- src/main/java/com/inw/filters/JwtRequestFilter.java
+++ src/main/java/com/inw/filters/JwtRequestFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -26,6 +27,7 @@ import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureException;;
 
 @Component
+@Order(2)
 public class JwtRequestFilter extends OncePerRequestFilter {
 
 	  	@Autowired
